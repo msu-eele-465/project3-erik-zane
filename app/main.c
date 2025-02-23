26994738ee164ade830369e1bb74b4633eb0d8d4
ZaneMLewis <zanelewis67@gmail.com>
2025-02-22 21:39:41 -0700

    changed heartbeat LED ISR to TB1

--- app/main.c
+++ app/main.c
@@ -1,32 +1,31 @@
-#include <msp430.h>
+#include <msp430.h> 
 
-void timerB0_init(void);
+void timerB1_init(void);
 
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;                   //stops watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;                   // Stops watchdog timer
 
+    P1DIR |= BIT0;                              // Sets P1.0 as an output
+    P1OUT &= ~BIT0;                             // Initializes LED to OFF
 
-    P1DIR |= BIT0;                              //sets P1.0 as an output
-    P1OUT &= ~BIT0;                             //initializes LED to OFF
+    PM5CTL0 &= ~LOCKLPM5;                       // Unlocks GPIO
 
-    PM5CTL0 &= ~LOCKLPM5;                       //unlocks GPIO
-
-    timerB0_init();                             //initializes B timer
+    timerB1_init();                             // Initializes B1 timer
 
     __bis_SR_register(LPM3_bits | GIE);         // LPM3 for ACLK usage, GIE enables interrupts, enter low power mode
 
     while (1) {
-                                                //main loop empty, ISR handles LED changeing
+                                                // Main loop empty, ISR handles LED toggling
     }
 }
 
-void timerB0_init(void) {
-    TB0CCTL0 = CCIE;                            //CCIE enables Timer B0 interrupt
-    TB0CCR0 = 32768;                            //sets Timer B0 to 1 second (32.768 kHz)
-    TB0CTL = TBSSEL_1 | ID_0 | MC_1 | TBCLR;    //ACLK, No divider, Up mode, Clear timer
+void timerB1_init(void) {
+    TB1CCTL0 = CCIE;                            // CCIE enables Timer B1 interrupt
+    TB1CCR0 = 32768;                            // Sets Timer B1 to 1 second (32.768 kHz)
+    TB1CTL = TBSSEL_1 | ID_0 | MC_1 | TBCLR;    // ACLK, No divider, Up mode, Clear timer
 }
 
-#pragma vector = TIMER0_B0_VECTOR               //time B0 ISR
-__interrupt void TIMERB0_ISR(void) {
-    P1OUT ^= BIT0;                              //toggles P1.0 LED
+#pragma vector = TIMER1_B0_VECTOR               // Timer B1 ISR
+__interrupt void TIMERB1_ISR(void) {
+    P1OUT ^= BIT0;                              // Toggles P1.0 LED
 }
