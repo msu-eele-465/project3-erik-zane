4eeeaf670884ff2cf86b7e5f9e0a282b3abe5531
ZaneMLewis <zanelewis67@gmail.com>
2025-02-23 01:18:24 -0700

    fixed colors

--- app/RGB.c
+++ app/RGB.c
@@ -16,21 +16,21 @@ void update_color(system_states new_state) { // system_states new_state) {
 
     switch (state) {
         case LOCKED:  // Red (#c43e1d)
-            red_counter = 210; // baseline 196
-            green_counter = 15; // baseline 62
-            blue_counter = 3; // baseline 29
+            red_counter = 255; // baseline 196
+            green_counter = 80; // baseline 62
+            blue_counter = 37; // baseline 29
             break;
 
         case UNLOCKING:  // Yellow (#c4921d)
-            red_counter = 170; // baseline 196
-            green_counter = 34; // baseline 146
-            blue_counter = 4; // baseline 29
+            red_counter = 255; // baseline 196
+            green_counter = 189; // baseline 146
+            blue_counter = 37; // baseline 29
             break;
 
         case UNLOCKED:  // Blue (#1da2c4)
-            red_counter = 29; // baseline 29
-            green_counter = 25; // baseline 162
-            blue_counter = 25; // baseline 196
+            red_counter = 37; // baseline 29
+            green_counter = 210; // baseline 162
+            blue_counter = 255; // baseline 196
             break;
     }
 }
